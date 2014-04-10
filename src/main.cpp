04ddbf90c918f239dd4f9717ffd0de497810110b
Vilmos Lőrincz <vilmos.lorincz@gmail.com>
2014-04-10 14:06:17 +0200

    típusvalidálás, parancssori mód

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,23 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	if (argv[1]) {
 		string action = argv[1];
+
 		string game_state_json;
-		cin >> game_state_json;
-
-		json::Value game_state = json::Deserialize(game_state_json);
-
 		if (action == "bet_request") {
+			if (argv[2]) {
+				game_state_json = argv[2];
+			}
+			else {
+				cin >> game_state_json;
+			}
+
+			json::Value game_state = json::Deserialize(game_state_json);
 			cout << Player::betRequest(game_state);
 		}
 		else if (action == "showdown") {
+			cin >> game_state_json;
+
+			json::Value game_state = json::Deserialize(game_state_json);
 			Player::showdown(game_state);
 		}
 		else if (action == "version") {
--- src/player.cpp
+++ src/player.cpp
@@ -1,12 +1,16 @@
 #include "player.h"
 
 
-const char *Player::VERSION = "hellraiser";
+const char *Player::VERSION = "hellraiser\n";
 
 int Player::betRequest(json::Value game_state) {
-	if (game_state.HasKey("minimum_raise")) {
+
+	if (game_state.GetType() == json::ValueType::ObjectVal && game_state.HasKey("minimum_raise")) {
 		json::Value out = game_state["minimum_raise"];
-		return out.ToInt();
+		if (out.GetType() == json::ValueType::IntVal) {
+			return out.ToInt();
+
+		}
 	}
 	return 10;
 }
